f3ccfb8066e8b1a6a75ebcba9cc1bd1fe93816ca
themonty <themonty@nate.com>
2021-11-30 22:56:13 +0900

    Update Enemy Move

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public enum State
+    public enum State : int
     {
         None = -1, // 사용전
         Ready = 0, // 준비완료
@@ -26,6 +26,12 @@ public class Enemy : MonoBehaviour
 
     [SerializeField]
     float CurrentSpeed;
+
+    Vector3 CurrentVelocity;
+
+    float MoveStartTime = 0.0f;
+
+    float BattleStarTime = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -35,22 +41,80 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            Appear(new Vector3(7.0f, transform.position.y, transform.position.z));
+        }       
+        
+        switch(CurrentState)
+        {
+            case State.None:
+                
+            case State.Ready:
+                break;
+            case State.Dead:
+                break;
+            case State.Appear:
+                
+            case State.Disappear:
+                UpdateSpeed();
+                UpdateMove();
+                break;
+            case State.Battle:
+                UpdateBattel();
+                break;
+
+        }
+
+        if (CurrentState == State.Appear || CurrentState == State.Disappear)
+        {
+            UpdateSpeed();
+            UpdateMove();
+        }
+
+        //CurrentState = State.Battle;
         
     }
 
     void UpdateSpeed()
     {
-
+        CurrentSpeed = Mathf.Lerp(CurrentSpeed, MaxSpeed, (Time.time - MoveStartTime)/MaxSpeedTime);
     }
 
     void UpdateMove()
     {
+        // 속도 = 거리 / 시간
+        // 시간 = 거리 / 속도
+        float distance = Vector3.Distance(TargetPosition, transform.position);
+        if(distance == 0)
+        {
+            Arrived();
+            return;
+        }
+
+        CurrentVelocity = (TargetPosition - transform.position).normalized * CurrentSpeed;
+
+        transform.position = Vector3.SmoothDamp(transform.position, TargetPosition, ref CurrentVelocity, distance / CurrentSpeed, MaxSpeed);
+        
+        if (CurrentState == State.Disappear)
+        {
+            transform.Rotate(Vector3.right * Time.deltaTime * 45);
+        }
 
     }
 
     void Arrived()
     {
-
+        CurrentSpeed = 0.0f;
+        if (CurrentState == State.Appear)
+        {
+            CurrentState = State.Battle;
+            BattleStarTime = Time.time;
+        }
+        else if (CurrentState == State.Disappear)
+        {
+            CurrentState = State.None;
+        }
     }
 
     public void Appear(Vector3 targetPos)
@@ -59,6 +123,8 @@ public class Enemy : MonoBehaviour
         CurrentSpeed = MaxSpeed;
         CurrentState = State.Appear;
 
+        MoveStartTime = Time.time;
+
     }
 
     void Disappear(Vector3 targetPos)
@@ -66,5 +132,15 @@ public class Enemy : MonoBehaviour
         TargetPosition = targetPos;
         CurrentSpeed = 0;
         CurrentState = State.Disappear;
+
+        MoveStartTime = 0.0f;
+    }
+
+    void UpdateBattel()
+    {
+        if (Time.time - BattleStarTime > 3.0f)
+        {
+            Disappear(new Vector3(-15.0f, transform.position.y, transform.position.z));
+        }
     }
 }
